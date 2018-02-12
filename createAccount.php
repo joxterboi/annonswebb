00f4f063ac0100251f5669ed9b5c41d06bb7ca93
joxterboi <joa123kim456@gmail.com>
2018-02-12 10:29:29 +0100

    Styling to signup page

--- createAccount.php
+++ createAccount.php
@@ -1,12 +1,13 @@
 <?php include 'components/header.php'; ?>
-
-<form id="signUpForm" action="includes/signup.inc.php" method="POST">
-    <input type="text" name="firstName" placeholder="Förnamn">
-    <input type="text" name="lastName" placeholder="Efternamn">
-    <input type="text" name="userEmail" placeholder="E-mail">
-    <input type="text" name="uid" placeholder="Användarnamn">
-    <input type="password" name="pwd" placeholder="Lösenord">
-    <button type="submit" name="signupSubmit">Sign up!</button>
-</form>
+<div id="signupContainer">
+    <form id="signUpForm" action="includes/signup.inc.php" method="POST">
+        <input type="text" name="firstName" placeholder="Förnamn"><br>
+        <input type="text" name="lastName" placeholder="Efternamn"><br>
+        <input type="text" name="userEmail" placeholder="E-mail"><br>
+        <input type="text" name="uid" placeholder="Användarnamn"><br>
+        <input type="password" name="pwd" placeholder="Lösenord"><br>
+        <button type="submit" name="signupSubmit">Sign up!</button>
+    </form>
+</div>
 
 <?php include 'components/footer.php'; ?>
