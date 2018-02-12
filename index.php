8c719701cc82882b6a576c9352c0d3a5e2a33867
joxterboi <joa123kim456@gmail.com>
2018-02-12 10:32:23 +0100

    Added the main titles to startpage

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php include 'components/header.php'; ?>
 <div id="hottestModels">
-
+    <h1>Hetaste modellerna</h1>
 </div>
 <div id="latestAds">
     <h1>Senaste annonserna</h1>
@@ -9,6 +9,6 @@
     ?>
 </div>
 <div id="hottestAds">
-
+    <h1>Hetaste annonserna</h1>
 </div>
 <?php include 'components/footer.php'; ?>
